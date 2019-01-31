eaa3ceee320aa239cc1c99631f25bb07d86d12e6
nielad <nielad@gmail.com>
2019-01-31 18:01:38 -0500

    Update Trie.java

--- Trie.java
+++ Trie.java
@@ -21,15 +21,41 @@ public class Trie {
     * by one if it already exists in the trie).
     * @param word - word to add to the trie.
     */
-   public void addWord(String word)
+   public void addWord(String word){
+	   
+	   if(s.isEmpty()){
+		this.word = true; 
+		return;
+	   }
+			
+	   char letter = s.charAt(0);
+	   int index = letter - 'a'; //'a' index of 97 b = 98, 98-97
+			
+	   if(this.children[index] == null){
+		this.children[index] = new Trie();
+	   }
+   }
+	 this.children[index].addWord(s.substring(1));  
 
    /**
     * @param word - query word to be searched.
     * @return - number of times word appears in file 
     * represented by the trie. 
     */
-   public int wordFrequency(String word)
-  
+   public int wordFrequency(String word){
+  if(s.isEmpty()){
+			return this.word;
+		}
+		char letter = s.charAt(0);
+		int index = letter - 'a';
+		
+		if(this.children[index] == null)
+		{
+			return false;
+		}
+		
+		return this.children[index].isWord(s.substring(1));
+   }
    /**
     * @return - Return the kth word in the sorted sequence
     * of unique words that exist in the file/trie,
