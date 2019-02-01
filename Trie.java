c0e33a09433f7211d20e41928f54edf33c56b5e4
nielad <nielad@gmail.com>
2019-02-01 15:39:08 -0500

    Update Trie.java

--- Trie.java
+++ Trie.java
@@ -1,69 +1,71 @@
-package trietest;
-import java.util.*; 
+
+import java.util.*;
 
 public class Trie {
- 
- char c;
-		Trie[] children;
-		boolean word;
- 
-   /**
-    * Construct an empty Trie with just a root node.
-    */
-   public Trie()
-    
-   	this.c=0;
-	this.children = new Trie[26];
-	this.word = false;
-           
-   /** 
-    * Adds a word to the trie (or increases its frequency 
-    * by one if it already exists in the trie).
-    * @param word - word to add to the trie.
-    */
-   public void addWord(String word){
-	   
-	   if(s.isEmpty()){
-		this.word = true; 
-		return;
-	   }
-			
-	   char letter = s.charAt(0);
-	   int index = letter - 'a'; //'a' index of 97 b = 98, 98-97
-			
-	   if(this.children[index] == null){
-		this.children[index] = new Trie();
-	   }
-   }
-	 this.children[index].addWord(s.substring(1));  
 
-   /**
-    * @param word - query word to be searched.
-    * @return - number of times word appears in file 
-    * represented by the trie. 
-    */
-   public int wordFrequency(String word){
-  if(s.isEmpty()){
-			return this.word;
-		}
-		char letter = s.charAt(0);
-		int index = letter - 'a';
-		
-		if(this.children[index] == null)
-		{
-			return false;
-		}
-		
-		return this.children[index].isWord(s.substring(1));
-   }
-   /**
-    * @return - Return the kth word in the sorted sequence
-    * of unique words that exist in the file/trie,
-    * where the first unique word is at position 1.
-    * (returns null if k exceeds the number unique
-    * words in the file).
-    * 
-    */
-   public String kthWord(int k)
-           
+    List<Trie> child = null;
+    boolean isLeaf;
+    int counter;
+
+    /**
+     * Construct an empty Trie with just a root node.
+     */
+    public Trie() {
+
+        isLeaf = false;
+        child = new ArrayList<>();
+
+        for (int i = 0; i < 26; i++) {
+            child.add(null);
+        }
+    }
+
+    /**
+     * Adds a word to the trie (or increases its frequency by one if it already
+     * exists in the trie).
+     *
+     * @param word - word to add to the trie.
+     */
+    public void addWord(String word) {
+
+        Trie current = this;
+
+        for (int i = 0; i < word.length(); i++) {
+            if (current.child.get(word.charAt(i) - 'a') == null) {
+                current.child.set(word.charAt(i) - 'a', new Trie());
+            }
+
+            current = current.child.get(word.charAt(i) - 'a');
+        }
+        current.isLeaf = true;
+
+    }
+
+    /**
+     * @param word - query word to be searched.
+     * @return - number of times word appears in file represented by the trie.
+     */
+
+    public int wordFrequency(String word) {
+        Trie current = this;
+
+        for (int i = 0; i < word.length(); i++) {
+            current = current.child.get(word.charAt(i) - 'a');
+
+            if (current == null) {
+                return 0;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * @return - Return the kth word in the sorted sequence of unique words that
+     * exist in the file/trie, where the first unique word is at position 1.
+     * (returns null if k exceeds the number unique words in the file).
+     *
+     */
+    public String kthWord(int k) {
+        return "";
+    }
 }
